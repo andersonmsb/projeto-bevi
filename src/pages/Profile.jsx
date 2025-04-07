19ead33c968f7c6adb1f9ef0157772af1b29102a
Anderson Barros <andersonmsbarros@gmail.com>
2025-04-07 13:12:45 -0300

    adicionado card produtos

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,6 +6,8 @@ import { useNavigate } from "react-router-dom";
 export default function Profile() {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
+  const [productCount, setProductCount] = useState(0);
+
 
   // inicio alerta estilizado centralizado
   const centerAlert = (icon, title, text) => {
@@ -51,7 +53,13 @@ export default function Profile() {
     api.post("/auth/me")
       .then((res) => setUser(res.data))
       .catch((err) => console.error(err));
+  
+    // Buscar produtos para contar
+    api.post("/product/list")
+      .then((res) => setProductCount(res.data.data.length))
+      .catch((err) => console.error(err));
   }, []);
+  
   // fim verifica autenticação
 
 
@@ -73,10 +81,19 @@ export default function Profile() {
       {/*fim titulo */}
       <h5 className="mb-5">Gerencie seus produtos, controle seu estoque e muito mais, <br/> de forma fácil e rápida.</h5>
 
+      {/* Card Total de Produtos */}
+      <div className="card shadow text-center mb-4 w-50 mx-auto">
+        <div className="card-body">
+          <h5 className="card-title fw-bold">Total de Produtos</h5>
+          <p className="fs-3 m-0">{productCount}</p>
+        </div>
+      </div>
+
+
       {/*Inicio button */}
       <div>
         <button type="submit" className="btn btn-primary btn-lg fw-bold" onClick={() => navigate("/produtos")}>
-          <i className="bi bi-bag me-1"></i>Acessar Produtos
+          <i className="bi bi-bag me-2"></i>Acessar Produtos
         </button>
       </div>
       {/*fim button */}
